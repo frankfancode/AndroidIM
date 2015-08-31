a89445901595a445dbc8849bbf35b37a9d074468
frankfancode <chinasdfxd@gmail.com>
2015-08-31 08:19:13 +0800

    第一次提交

---
+++ ImServer/src/com/frankfann/net/server/Client.java
@@ -0,0 +1,113 @@
+package com.frankfann.net.server;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
+public class Client {
+
+    JTextArea incoming;
+    JTextField outgoing;
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket sock;
+
+    public static void main(String[] args) {
+    	Client client = new Client();
+       client.go();
+    }
+
+   public void go() {
+
+       // build gui
+
+        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        
+        incoming = new JTextArea(15,20);  // was 15,50
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+       
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        
+
+        outgoing = new JTextField(20);
+        
+
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+
+        
+        mainPanel.add(qScroller);
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        
+        setUpNetworking();
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+          
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(400,500);
+        frame.setVisible(true);
+                
+   
+     } // close go
+
+   private void setUpNetworking() {  
+        try {
+           sock = new Socket("127.0.0.1", 5000);
+           InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+           reader = new BufferedReader(streamReader);
+
+           writer = new PrintWriter(sock.getOutputStream());
+          
+           System.out.println("networking established");
+        } catch(IOException ex) {
+           ex.printStackTrace();
+        }
+     } // close setUpNetworking   
+
+   public class SendButtonListener implements ActionListener {
+      public void actionPerformed(ActionEvent ev) {
+          try {
+             writer.println(outgoing.getText());
+             writer.flush();
+             
+          } catch(Exception ex) {
+             ex.printStackTrace();
+          }
+          outgoing.setText("");
+          outgoing.requestFocus();
+      }
+    }  // close SendButtonListener inner class
+
+  public class IncomingReader implements Runnable {
+     public void run() {
+           String message;             
+           try {
+
+             while ((message = reader.readLine()) != null) {                        
+                System.out.println("read " + message);
+                incoming.append(message + "\n");
+    
+              } // close while
+           } catch(Exception ex) {ex.printStackTrace();}
+       } // close run
+   } // close inner class     
+}
---
+++ ImServer/src/com/frankfann/net/server/Server.java
@@ -0,0 +1,94 @@
+package com.frankfann.net.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Server {
+
+    ArrayList<PrintWriter> clientOutputStreams;
+
+
+    public class ClientHandler implements Runnable {
+
+         BufferedReader reader;
+       
+         Socket sock;
+   
+
+         public ClientHandler(Socket clientSocket) {
+           try {
+             sock = clientSocket;
+             InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+             reader = new BufferedReader(isReader);
+              
+           } catch(Exception ex) {ex.printStackTrace();}
+          } // close constructor
+
+        public void run() {
+           String message;
+             
+           try {
+
+             while ((message = reader.readLine()) != null) {
+                        
+                System.out.println("read " + message);
+                tellEveryone(message);
+    
+              } // close while
+           } catch(Exception ex) {ex.printStackTrace();}
+       } // close run
+   } // close inner class
+      
+
+
+
+     public static void main (String[] args) {
+         new Server().go();
+    }
+
+     public void go() {
+      clientOutputStreams = new ArrayList<PrintWriter>();
+
+       try {
+       ServerSocket serverSock = new ServerSocket(5000);
+
+       while(true) {
+          Socket clientSocket = serverSock.accept();
+          PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());         
+          clientOutputStreams.add(writer);
+
+       Thread t = new Thread(new ClientHandler(clientSocket));
+       t.start();
+
+
+       System.out.println("got a connection");
+     }
+       // now if I get here I have a connection
+               
+      }catch(Exception ex) {
+         ex.printStackTrace();
+      }
+   }
+
+   public void tellEveryone(String message) {
+      Iterator it = clientOutputStreams.iterator();
+      while(it.hasNext()) {
+        try {
+           PrintWriter writer = (PrintWriter) it.next();
+           writer.println(message);
+           writer.flush();
+         } catch(Exception ex) {
+              ex.printStackTrace();
+         }
+      
+       } // end while
+       
+   } // close tellEveryone
+
+}
