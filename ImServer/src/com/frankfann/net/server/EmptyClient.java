4d08b2fa403d14b9bb86d7ea076be47d17f0d8f4
frankfancode <chinasdfxd@gmail.com>
2015-09-01 11:19:34 +0800

    no message

---
+++ ImServer/src/com/frankfann/net/server/EmptyClient.java
@@ -0,0 +1,45 @@
+package com.frankfann.net.server;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_10;
+import org.java_websocket.handshake.ServerHandshake;
+
+public class EmptyClient extends WebSocketClient {
+
+    public EmptyClient(URI serverUri, Draft draft) {
+        super(serverUri, draft);
+    }
+
+    public EmptyClient(URI serverURI) {
+        super(serverURI);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        System.out.println("new connection opened");
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("closed with exit code " + code + " additional info: " + reason);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println("received message: " + message);
+    }
+
+    @Override
+    public void onError(Exception ex) {
+        System.err.println("an error occurred:" + ex);
+    }
+
+    public static void main(String[] args) throws URISyntaxException {      
+        WebSocketClient client = new EmptyClient(new URI("ws://localhost:8887"), new Draft_10());
+        client.connect();
+    }
+}
